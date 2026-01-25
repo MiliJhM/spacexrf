831aff4241e3bebdfc561e7b2c5227c1d908df4e
Ma Jiahao <598282085@qq.com>
2026-01-25 17:21:09 +0800

    Fix message formatting for initial weights logging

--- R/platform_effect_normalization.R
+++ R/platform_effect_normalization.R
@@ -88,7 +88,7 @@ choose_sigma_c <- function(RCTD) {
   message(paste('chooseSigma: using initial Q_mat with sigma = ',sigma/100))
   for(iter in 1:RCTD@config$N_epoch) {
     set_likelihood_vars(Q_mat_all[[as.character(sigma)]], X_vals)
-    #message(paste('chooseSigma: getting initial weights for #samples: ',N_fit))
+    message(paste('chooseSigma: getting initial weights for #samples: ',N_fit))
     results = decompose_batch(puck@nUMI[fit_ind], RCTD@cell_type_info$renorm[[1]], beads, RCTD@internal_vars$gene_list_reg, constrain = F, max_cores = RCTD@config$max_cores)
     weights = Matrix(0, nrow = N_fit, ncol = RCTD@cell_type_info$renorm[[3]])
     rownames(weights) = fit_ind; colnames(weights) = RCTD@cell_type_info$renorm[[2]];
